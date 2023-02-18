18da83ccc0b671b713e4bd0206c72f37ed57b71e
ofadiman <ofadiman@gmail.com>
2023-02-18 11:40:16 +0100

    feat: set active column style

--- main.go
+++ main.go
@@ -42,21 +42,41 @@ func (p PullRequest) Description() string {
 	return "desc"
 }
 
+type Column string
+
+const (
+	Waiting Column = "waiting"
+	Checked Column = "checked"
+)
+
 type Model struct {
-	waiting list.Model
-	checked list.Model
+	activeColumn Column
+	waiting      list.Model
+	checked      list.Model
 }
 
-func (m Model) Init() tea.Cmd {
+func (m *Model) ToggleActiveColumn() {
+	if m.activeColumn == Checked {
+		m.activeColumn = Waiting
+	} else {
+		m.activeColumn = Checked
+	}
+}
+
+func (m *Model) Init() tea.Cmd {
 	return nil
 }
 
-var columnStyle = lipgloss.
+var defaultColumnStyle = lipgloss.
 	NewStyle().
-	BorderStyle(lipgloss.RoundedBorder()).
+	Border(lipgloss.HiddenBorder())
+
+var activeColumnStyle = lipgloss.
+	NewStyle().
+	Border(lipgloss.RoundedBorder()).
 	BorderForeground(lipgloss.Color("63"))
 
-func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		{
@@ -75,6 +95,18 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				{
 				}
 
+			case "left", "h":
+				{
+					m.ToggleActiveColumn()
+					return m, nil
+				}
+
+			case "right", "l":
+				{
+					m.ToggleActiveColumn()
+					return m, nil
+				}
+
 			case "enter", " ":
 				{
 				}
@@ -82,8 +114,9 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 	case tea.WindowSizeMsg:
 		{
-			horizontalFrameSize, verticalFrameSize := columnStyle.GetFrameSize()
-			columnStyle.Width(msg.Width/2 - horizontalFrameSize)
+			horizontalFrameSize, verticalFrameSize := defaultColumnStyle.GetFrameSize()
+			defaultColumnStyle.Width(msg.Width/2 - horizontalFrameSize)
+			activeColumnStyle.Width(msg.Width/2 - horizontalFrameSize)
 			debug(strconv.Itoa(msg.Width))
 
 			m.waiting = list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
@@ -118,12 +151,20 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
-func (m Model) View() string {
-	return lipgloss.JoinHorizontal(lipgloss.Left, columnStyle.Render(m.waiting.View()), columnStyle.Render(m.checked.View()))
+func (m *Model) View() string {
+	if m.activeColumn == Waiting {
+		return lipgloss.JoinHorizontal(lipgloss.Left, activeColumnStyle.Render(m.waiting.View()), defaultColumnStyle.Render(m.checked.View()))
+	}
+
+	if m.activeColumn == Checked {
+		return lipgloss.JoinHorizontal(lipgloss.Left, defaultColumnStyle.Render(m.waiting.View()), activeColumnStyle.Render(m.checked.View()))
+	}
+
+	panic("invalid activeColumn value")
 }
 
 func main() {
-	program := tea.NewProgram(Model{}, tea.WithAltScreen())
+	program := tea.NewProgram(&Model{activeColumn: Waiting}, tea.WithAltScreen())
 	if _, err := program.Run(); err != nil {
 		panic(err)
 	}
