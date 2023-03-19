b798b574dcaf964f8d39676b37d19d6f51c1f9a2
ofadiman <ofadiman@gmail.com>
2023-03-19 21:42:32 +0100

    feature: help component

--- pull_requests_screen.go
+++ pull_requests_screen.go
@@ -1,6 +1,16 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/muesli/reflow/padding"
+	"github.com/muesli/reflow/wordwrap"
+	"strings"
+)
+
+var roundedBorder = lipgloss.RoundedBorder()
+var columnStyle = lipgloss.NewStyle().Border(roundedBorder).BorderForeground(lipgloss.Color("63"))
 
 type PullRequestsScreenModel struct {
 	WindowWidth  int
@@ -29,5 +39,42 @@ func (r *PullRequestsScreenModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (r *PullRequestsScreenModel) View() string {
-	return "renders pull requests screen"
+	columnStyle.Width(r.WindowWidth - roundedBorder.GetLeftSize() - roundedBorder.GetRightSize())
+	header := columnStyle.Render("renders pull requests screen")
+	lorem := "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+	f := wordwrap.NewWriter(r.WindowWidth - roundedBorder.GetLeftSize() - roundedBorder.GetRightSize())
+	f.Breakpoints = []rune{' '}
+	_, err := f.Write([]byte(lorem))
+	if err != nil {
+		debug.msg(debug.Error(), err.Error())
+	}
+
+	help := []struct {
+		shortcut    string
+		description string
+	}{
+		{
+			shortcut:    "ctrl + q",
+			description: "quit",
+		},
+		{
+			shortcut:    "ctrl + s",
+			description: "settings",
+		},
+		{
+			shortcut:    "ctrl + p",
+			description: "pull requests",
+		},
+	}
+
+	description := lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	shortcut := lipgloss.NewStyle().Foreground(lipgloss.Color("250"))
+
+	mapped := make([]string, len(help))
+	for i, value := range help {
+		mapped[i] = padding.String(fmt.Sprintf("%v %v", shortcut.Render(value.shortcut), description.Render(value.description)), 20)
+	}
+
+	return lipgloss.JoinVertical(0, header, columnStyle.Render(f.String()), strings.Join(mapped, ""))
 }
