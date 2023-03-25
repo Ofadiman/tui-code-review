089d9780321c7757641bb2e183941462c2b610ef
ofadiman <ofadiman@gmail.com>
2023-03-25 09:22:55 +0100

    chore: cleanup logger struct from comments

--- log/logger.go
+++ log/logger.go
@@ -24,7 +24,7 @@ func NewLogger() *Logger {
 	return logger
 }
 
-func (r *Logger) Append(msg string) {
+func (r *Logger) append(msg string) {
 	file, _ := os.OpenFile(r.file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	defer file.Close()
 
@@ -32,29 +32,17 @@ func (r *Logger) Append(msg string) {
 }
 
 func (r *Logger) Error(error error) {
-	r.Append(fmt.Sprintf("error: %v", error))
+	r.append(fmt.Sprintf("error: %v", error))
 }
 
 func (r *Logger) Info(msg string) {
-	r.Append(fmt.Sprintf("info: %v", msg))
-	//err := os.WriteFile(r.file, []byte(fmt.Sprintf("info: %v", msg)), 0644)
-	//if err != nil {
-	//	panic(err)
-	//}
+	r.append(fmt.Sprintf("info: %v", msg))
 }
 
 func (r *Logger) Struct(msg any) {
-	r.Append(fmt.Sprintf("struct: %v", msg))
-	//err := os.WriteFile(r.file, []byte(fmt.Sprintf("struct: %v", msg)), 0644)
-	//if err != nil {
-	//	panic(err)
-	//}
+	r.append(fmt.Sprintf("struct: %v", msg))
 }
 
 func (r *Logger) KeyPress(msg string) {
-	r.Append(fmt.Sprintf("key_press: %v", msg))
-	//err := os.WriteFile(r.file, []byte(fmt.Sprintf("key_press: %v", msg)), 0644)
-	//if err != nil {
-	//	panic(err)
-	//}
+	r.append(fmt.Sprintf("key_press: %v", msg))
 }
